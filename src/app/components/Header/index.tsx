ba9f0cbd52892f9a088cf99a877630ed54abd0c5
manumac86 <emmartinez86@gmail.com>
2023-08-19 15:48:23 +0200

    Add layout. Add react-query

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import Logo from 'src/images/logo.png'
+import styles from './styles.module.css'
+
+export default function Header() {
+  return (
+    <main>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <Image src={Logo} alt="Family Portrait" width={100} height={80} />
+          <h1>Family Portrait</h1>
+        </div>
+        <nav>
+          <ul className={styles.nav}>
+            <li> <a href="/">Home</a> </li>
+            <li> <a href="/team">Team</a> </li>
+            <li> <a href="/help">FAQ</a> </li>
+          </ul>
+        </nav>
+      </header>
+    </main>
+  )
+}
---
+++ src/app/help/page.tsx
@@ -0,0 +1,7 @@
+export default function Help() {
+  return (
+    <main>
+      <h1>FAQ</h1>
+    </main>
+  )
+}
